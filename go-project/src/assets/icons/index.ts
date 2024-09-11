481009d986880cb07d38bde9d0d283157ba2d12d
Hagan7854 <Haganfelix43@gmail.com>
2024-09-11 14:17:44 +0000

    added an image to the community list

--- go-project/src/assets/icons/index.ts
+++ go-project/src/assets/icons/index.ts
@@ -4,3 +4,4 @@ export { default as AppleStoreLogo } from "./apple_store.svg";
 export { default as Person } from "./person.svg";
 export { default as Key } from "./key.svg";
 export { default as ProfilePic } from "./picture.svg";
+export { default as Communtiy } from "./exercise.svg";
--- go-project/src/components/page/dashboard/main.tsx
+++ go-project/src/components/page/dashboard/main.tsx
@@ -1,7 +1,16 @@
+import Community from "../communities/index"
 
 const MyCommunities = () => {
   return (
-    <div className="ml-64 bg-[#F8F8F8] h-screen">main</div>
+    <div className="ml-64 bg-[#F8F8F8] h-full">
+      <div className="pt-5 pl-5">
+      <h1>
+        My Communities
+      </h1>
+      <Community/>
+      </div>
+      
+    </div>
   )
 }
 
