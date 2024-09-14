fb0dd2cca308a6f45c2897406cd5c054462a8582
Hagan7854 <Haganfelix43@gmail.com>
2024-09-14 23:40:09 +0000

    added a progress bar file

--- go-project/src/components/page/communities/index.tsx
+++ go-project/src/components/page/communities/index.tsx
@@ -1,17 +1,29 @@
 import { Communtiy } from "@/assets/icons";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 
 export default function Community() {
-return <div>
-    <section className="flex flex-wrap justify-between gap-y-7">
-        <div className="grid grid-rows-5 w-72 h-96">
-        <img src={Communtiy} alt="community image" className=" row-span-2 bg-gray-300 rounded-3xl object-cover "/>
-            <div className="row-span-3 bg-red-500">
-
-            community
+  return (
+    <div>
+      <section className="flex flex-wrap justify-between gap-y-7">
+        <div className="flex flex-col w-72 h-96 bg-white rounded-3xl">
+          <img
+            src={Communtiy}
+            alt="community image"
+            className=" rounded-3xl object-cover"
+          />
+          <div className="flex flex-col px-4 my-3">
+            <div className="flex justify-between items-end">
+                <div className="flex flex-col">
+                    <p className="text-base font-bold text-gray-400 ">Fitness</p>
+                    <p className="text-lg font-bold text-green-600 ">Jarul Fitness</p>
+                </div>
+                <Button className="bg-green-500 rounded-3xl py-0 h-8 w-20 font-bold text-base">Open</Button>
             </div>
+            <Progress value={45} indicatorColor="bg-green-600"/>
+          </div>
         </div>
-       
-        
-    </section>
-</div>
+      </section>
+    </div>
+  );
 }
--- go-project/src/components/page/dashboard/main.tsx
+++ go-project/src/components/page/dashboard/main.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 
 const MyCommunities = () => {
   return (
-    <div className="ml-64 bg-[#F8F8F8] h-full">
+    <div className="ml-64 bg-[#F5F5F5] h-screen">
       <div className="pt-5 pl-5">
       <h1>
         My Communities
