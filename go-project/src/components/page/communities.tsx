102b776153f1b0775e17f1ff8224f474730d8e54
Hagan7854 <Haganfelix43@gmail.com>
2024-09-15 08:04:56 +0000

    fixed a bug

--- go-project/src/components/page/communities.tsx
+++ go-project/src/components/page/communities.tsx
@@ -4,7 +4,7 @@ import SideBar from "./side-nav"
 
 const Communities = () => {
   return (
-    <div className="flex">
+    <div className="flex bg-[#F5F5F5] h-full">
       <div>
       <div className="navbar bg-green-500 h-16 w-64 flex justify-center items-center ">
         <img src={GoLogo} alt="logo" className="w-32" />
