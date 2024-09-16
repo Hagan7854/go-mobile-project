1d5675d123977b90548ac304c1c82c02738ea794
Hagan7854 <Haganfelix43@gmail.com>
2024-09-16 10:55:57 +0000

    fixed a bug

--- go-project/src/components/page/communities/index.tsx
+++ go-project/src/components/page/communities/index.tsx
@@ -38,7 +38,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -87,7 +87,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -136,7 +136,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -185,7 +185,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -234,7 +234,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -283,7 +283,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -332,7 +332,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -381,7 +381,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -430,7 +430,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -479,7 +479,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -528,7 +528,7 @@ export default function Community() {
                     </div>
                 </div>
             </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
+            <Progress value={45} indicatorColor="bg-green-600" className="h-3 bg-green-200"/>
             <div className="flex flex-row justify-between text-green-600 text-[12px]">
                 <p>45%</p>
                 <p>Rating</p>
@@ -546,202 +546,7 @@ export default function Community() {
             <div className="w-full h-2 bg-gray-300 mt-2"></div>
           </div>
         </div>
-        <div className="flex flex-col w-72 h-96 bg-white rounded-3xl">
-          <img
-            src={Communtiy}
-            alt="community image"
-            className=" rounded-3xl object-cover"
-          />
-          <div className="flex flex-col px-4 my-3 gap-1">
-            <div className="flex justify-between items-end">
-                <div className="flex flex-col">
-                    <p className="text-base font-bold text-gray-400 ">Fitness</p>
-                    <p className="text-lg font-bold text-green-600 ">Jarul Fitness</p>
-                </div>
-                <Button className="bg-green-500 rounded-3xl py-0 h-8 w-20 font-bold text-base">Open</Button>
-            </div>
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-row items-start gap-2">
-                    <FaUsers/>
-                    <div className="text-[12px]">
-                        <p>47</p>
-                        <p>Members</p>
-                        </div>
-                    
-                </div>
-                <div className="flex flex-row items-center gap-1 text-[12px]">
-                    <FaClock/>
-                    <div>
-                        <span className="font-bold mr-1">1</span>
-                        <span>year</span>
-                    </div>
-                </div>
-            </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
-            <div className="flex flex-row justify-between text-green-600 text-[12px]">
-                <p>45%</p>
-                <p>Rating</p>
-            </div>
-            <p className="text-[12px] font-bold">UPCOMING EVENT</p>
-            <div className="flex flex-row justify-between items-center">
-                <p className="text-[9px] font-bold">2024.05.27</p>
-                <div className="flex flex-row gap-2 items-center text-[9px] text-gray-400">
-                    <span>Physio,</span>
-                    <span>Session,</span>
-                    <span>Lamda Court,</span>
-                    <span>Spintex</span>
-                </div>
-            </div>
-            <div className="w-full h-2 bg-gray-300 mt-2"></div>
-          </div>
-        </div>
-        <div className="flex flex-col w-72 h-96 bg-white rounded-3xl">
-          <img
-            src={Communtiy}
-            alt="community image"
-            className=" rounded-3xl object-cover"
-          />
-          <div className="flex flex-col px-4 my-3 gap-1">
-            <div className="flex justify-between items-end">
-                <div className="flex flex-col">
-                    <p className="text-base font-bold text-gray-400 ">Fitness</p>
-                    <p className="text-lg font-bold text-green-600 ">Jarul Fitness</p>
-                </div>
-                <Button className="bg-green-500 rounded-3xl py-0 h-8 w-20 font-bold text-base">Open</Button>
-            </div>
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-row items-start gap-2">
-                    <FaUsers/>
-                    <div className="text-[12px]">
-                        <p>47</p>
-                        <p>Members</p>
-                        </div>
-                    
-                </div>
-                <div className="flex flex-row items-center gap-1 text-[12px]">
-                    <FaClock/>
-                    <div>
-                        <span className="font-bold mr-1">1</span>
-                        <span>year</span>
-                    </div>
-                </div>
-            </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
-            <div className="flex flex-row justify-between text-green-600 text-[12px]">
-                <p>45%</p>
-                <p>Rating</p>
-            </div>
-            <p className="text-[12px] font-bold">UPCOMING EVENT</p>
-            <div className="flex flex-row justify-between items-center">
-                <p className="text-[9px] font-bold">2024.05.27</p>
-                <div className="flex flex-row gap-2 items-center text-[9px] text-gray-400">
-                    <span>Physio,</span>
-                    <span>Session,</span>
-                    <span>Lamda Court,</span>
-                    <span>Spintex</span>
-                </div>
-            </div>
-            <div className="w-full h-2 bg-gray-300 mt-2"></div>
-          </div>
-        </div>
-        <div className="flex flex-col w-72 h-96 bg-white rounded-3xl">
-          <img
-            src={Communtiy}
-            alt="community image"
-            className=" rounded-3xl object-cover"
-          />
-          <div className="flex flex-col px-4 my-3 gap-1">
-            <div className="flex justify-between items-end">
-                <div className="flex flex-col">
-                    <p className="text-base font-bold text-gray-400 ">Fitness</p>
-                    <p className="text-lg font-bold text-green-600 ">Jarul Fitness</p>
-                </div>
-                <Button className="bg-green-500 rounded-3xl py-0 h-8 w-20 font-bold text-base">Open</Button>
-            </div>
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-row items-start gap-2">
-                    <FaUsers/>
-                    <div className="text-[12px]">
-                        <p>47</p>
-                        <p>Members</p>
-                        </div>
-                    
-                </div>
-                <div className="flex flex-row items-center gap-1 text-[12px]">
-                    <FaClock/>
-                    <div>
-                        <span className="font-bold mr-1">1</span>
-                        <span>year</span>
-                    </div>
-                </div>
-            </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
-            <div className="flex flex-row justify-between text-green-600 text-[12px]">
-                <p>45%</p>
-                <p>Rating</p>
-            </div>
-            <p className="text-[12px] font-bold">UPCOMING EVENT</p>
-            <div className="flex flex-row justify-between items-center">
-                <p className="text-[9px] font-bold">2024.05.27</p>
-                <div className="flex flex-row gap-2 items-center text-[9px] text-gray-400">
-                    <span>Physio,</span>
-                    <span>Session,</span>
-                    <span>Lamda Court,</span>
-                    <span>Spintex</span>
-                </div>
-            </div>
-            <div className="w-full h-2 bg-gray-300 mt-2"></div>
-          </div>
-        </div>
-        <div className="flex flex-col w-72 h-96 bg-white rounded-3xl">
-          <img
-            src={Communtiy}
-            alt="community image"
-            className=" rounded-3xl object-cover"
-          />
-          <div className="flex flex-col px-4 my-3 gap-1">
-            <div className="flex justify-between items-end">
-                <div className="flex flex-col">
-                    <p className="text-base font-bold text-gray-400 ">Fitness</p>
-                    <p className="text-lg font-bold text-green-600 ">Jarul Fitness</p>
-                </div>
-                <Button className="bg-green-500 rounded-3xl py-0 h-8 w-20 font-bold text-base">Open</Button>
-            </div>
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-row items-start gap-2">
-                    <FaUsers/>
-                    <div className="text-[12px]">
-                        <p>47</p>
-                        <p>Members</p>
-                        </div>
-                    
-                </div>
-                <div className="flex flex-row items-center gap-1 text-[12px]">
-                    <FaClock/>
-                    <div>
-                        <span className="font-bold mr-1">1</span>
-                        <span>year</span>
-                    </div>
-                </div>
-            </div>
-            <Progress value={45} indicatorColor="bg-green-600" className="h-3"/>
-            <div className="flex flex-row justify-between text-green-600 text-[12px]">
-                <p>45%</p>
-                <p>Rating</p>
-            </div>
-            <p className="text-[12px] font-bold">UPCOMING EVENT</p>
-            <div className="flex flex-row justify-between items-center">
-                <p className="text-[9px] font-bold">2024.05.27</p>
-                <div className="flex flex-row gap-2 items-center text-[9px] text-gray-400">
-                    <span>Physio,</span>
-                    <span>Session,</span>
-                    <span>Lamda Court,</span>
-                    <span>Spintex</span>
-                </div>
-            </div>
-            <div className="w-full h-2 bg-gray-300 mt-2"></div>
-          </div>
-        </div>
+
       </section>
     </div>
   );
